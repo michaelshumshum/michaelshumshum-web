e561e1df961cb958d84840f710e80f8b5e1a6e86
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2024-05-19 21:43:12 +0800

    footerlink to github page

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,35 +1,91 @@
 <script lang="ts">
-  import svelteLogo from "../assets/svelte.svg";
+  import { onMount } from "svelte";
+
+  let footerLink: HTMLAnchorElement;
+  let footerLinkShadow: HTMLHeadingElement;
+
+  const enter = () => {
+    (
+      footerLink.querySelectorAll(":not(h2)") as NodeListOf<HTMLElement>
+    ).forEach((element) => {
+      element.style.opacity = "0";
+      element.style.transform = "translateX(100%)";
+    });
+    footerLinkShadow.style.opacity = "1";
+    footerLinkShadow.style.transform = "translateY(0%)";
+  };
+
+  const leave = () => {
+    (
+      footerLink.querySelectorAll(":not(h2)") as NodeListOf<HTMLElement>
+    ).forEach((element) => {
+      element.style.opacity = "1";
+      element.style.transform = "translateX(0%)";
+    });
+    footerLinkShadow.style.opacity = "0";
+    footerLinkShadow.style.transform = "translateY(100%)";
+  };
+
+  onMount(() => {
+    leave();
+  });
 </script>
 
 <footer>
-  <p>Created with</p>
-  <img src="/svelte.svg" alt="svelte" />
-  <img src="/typescript.svg" alt="typescript" />
-  <img src="/vite.svg" alt="vite" />
+  <a
+    bind:this={footerLink}
+    on:mouseenter={enter}
+    on:mouseleave={leave}
+    target="_blank"
+    href="https://github.com/michaelshumshum/michaelshumshum-web"
+  >
+    <p>Created with</p>
+    <img draggable="false" src="/svelte.svg" alt="svelte" />
+    <img draggable="false" src="/typescript.svg" alt="typescript" />
+    <img draggable="false" src="/vite.svg" alt="vite" />
+    <h2 bind:this={footerLinkShadow}>View Source</h2>
+  </a>
 </footer>
 
 <style>
   footer {
     border-top: 2px solid black;
     background: white;
-    gap: 1vw;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: flex-end;
     height: 10vh;
     padding: 0 5vw;
     font-weight: 900;
     width: 100%;
   }
 
-  footer * {
-    user-select: none;
-    pointer-events: none;
+  footer a {
+    display: block;
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-end;
+    height: inherit;
+    gap: 1vw;
+    text-decoration: none;
+    color: black;
   }
 
-  footer img {
+  footer a *:not(h2) {
+    transition: all 200ms ease;
+  }
+
+  footer a h2 {
+    transition: all 200ms ease;
+    position: absolute;
+    font-size: 1.5em;
+    border-bottom: 2px solid black;
+  }
+
+  footer a * {
+    user-select: none;
+  }
+
+  footer a img {
     height: 40%;
   }
 </style>

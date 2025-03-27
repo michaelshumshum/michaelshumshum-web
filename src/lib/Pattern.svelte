1770ba1a4c8314675a837c63374c979281e416ae
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2025-03-27 16:38:23 -0700

    use canvas instead of stupid img

--- src/lib/Pattern.svelte
+++ src/lib/Pattern.svelte
@@ -1,125 +1,71 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { windowSize } from "./common/stores";
-  import { get } from "svelte/store";
   import { incrementLoadingSemaphore } from "./common/utils";
 
   const decrement = incrementLoadingSemaphore();
 
   const _font_size = 8;
-  const _text_length = 500000;
+  const _text_length = 500;
 
   export let baseText: string;
 
-  const textures: string[] = [];
-  let img: HTMLImageElement;
-  let currentTexture = 0;
+  const texts: string[] = [];
+  let canvas: HTMLCanvasElement;
+  let currentIndex = 0;
 
-  function loadTexturesFromCache() {
-    const currentWindowSize = get(windowSize);
-    const lastWindowSize = {
-      width: 0,
-      height: 0,
-    };
-    const _ = window.localStorage.getItem("last_window_size");
-    if (!_) {
-      return false;
+  async function createTexts() {
+    const text = `${baseText} `.repeat(_text_length / baseText.length);
+    for (let i = 0; i < baseText.length + 1; i++) {
+      texts.push(text.slice(i, i + _text_length));
     }
-    lastWindowSize.width = Number.parseInt(_.split(",")[0], 16);
-    lastWindowSize.height = Number.parseInt(_.split(",")[1], 16);
-    let localStorageItem = window.localStorage.getItem(`pattern_${baseText}_0`);
-
-    if (
-      currentWindowSize.width !== lastWindowSize.width ||
-      currentWindowSize.height !== lastWindowSize.height
-    ) {
-      return false;
-    }
-
-    if (localStorageItem) {
-      textures.push(localStorageItem);
-
-      let i = 1;
-      while (true) {
-        localStorageItem = window.localStorage.getItem(
-          `pattern_${baseText}_${i}`,
-        );
-        if (localStorageItem) {
-          textures.push(localStorageItem);
-          i++;
-        } else {
-          break;
-        }
-      }
-
-      return true;
-    }
-    return false; // no textures found in cache
-  }
-
-  async function generateTextures() {
-    // caching the textures to reduce loading on subsequent visits.
-    if (loadTexturesFromCache()) {
-      return;
-    }
-    let text = `${baseText} `.repeat(_text_length / 15);
-    const tempCanvas = document.createElement("canvas");
-    const tempCtx = tempCanvas.getContext("2d");
-    if (!tempCtx) {
-      console.error("Could not get 2d context of tempCanvas");
-      return;
-    }
-    tempCanvas.width = get(windowSize).width;
-    tempCanvas.height = get(windowSize).height;
-
-    tempCtx.font = `${_font_size}px monospace`;
-    tempCtx.fillStyle = "rgba(0,0,0,0.2)";
-
-    for (let _ = 0; _ < baseText.length + 1; _++) {
-      text = text.slice(1) + text[0];
-      tempCtx.clearRect(0, 0, tempCanvas.width, tempCanvas.height);
-
-      const necessaryLines = Math.ceil(tempCanvas.height / _font_size);
-      const lineLength = Math.ceil(_text_length / necessaryLines);
-
-      for (let i = 0; i < necessaryLines; i++) {
-        const line = text.slice(i * lineLength, (i + 1) * lineLength);
-        tempCtx.fillText(line, 0, (i + 1) * _font_size);
-      }
-      textures.push(tempCanvas.toDataURL("image/webp", 0.2));
-    }
-    for (let i = 0; i < textures.length; i++) {
-      window.localStorage.setItem(`pattern_${baseText}_${i}`, textures[i]);
-    }
-
-    // store the current window size
-    window.localStorage.setItem(
-      "last_window_size",
-      `${get(windowSize).width.toString(16)},${get(windowSize).height.toString(16)}`,
-    );
   }
 
   function animation() {
-    // TODO: when resizing, the image is stretched or shrank, which results in a unintended pattern look
-    // regenerating the textures on windowSize change is a possible solution, but it results in a lag spike as a result of the heavy graphics computation
-    currentTexture = (currentTexture + 1) % textures.length;
-    img.src = textures[currentTexture];
+    currentIndex = (currentIndex + 1) % texts.length;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) {
+      return;
+    }
+
+    // clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "rgba(0,0,0,0.2)";
+    ctx.font = `${_font_size * window.devicePixelRatio}px monospace`;
+    for (let i = 0; i < Math.ceil(canvas.height / _font_size); i++) {
+      ctx.fillText(
+        texts[(currentIndex + i) % texts.length],
+        0,
+        i * _font_size * window.devicePixelRatio,
+      );
+    }
 
     setTimeout(() => {
       requestAnimationFrame(animation);
-    }, 75);
+    }, 100);
   }
 
   onMount(() => {
-    generateTextures().then(decrement);
+    createTexts().then(() => {
+      decrement();
+      animation();
+    });
+    windowSize.subscribe((w) => {
+      if (canvas) {
+        canvas.width = w.width * window.devicePixelRatio;
+        canvas.height = w.height * window.devicePixelRatio;
+      }
+    });
     animation();
   });
 </script>
 
-<img alt="" src="" bind:this={img} draggable="false" />
+<canvas bind:this={canvas} />
 
 <style>
-  img {
+  canvas {
     position: absolute;
     left: 0;
     top: 0;
